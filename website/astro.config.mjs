a0f7e85d4604854fcb7c1aaf3d8878375e5997bc
clickglue <clickglue@telenet.be>
2023-01-04 23:28:33 +0100

    Update astro.config.mjs

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -30,8 +30,8 @@ export default defineConfig({
     mdx(options),
     tailwind(),
   ],
-  site: `https://strudel.tidalcycles.org`,
-  base: '/',
+  site: `https://clickglue.github.io/strudel`,
+  base: '/strudel',
 });
 
 /*
